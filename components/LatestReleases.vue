6dd4c7a2cb87d075dc20f188ad4ceaaa8d2849c1
firemasterleo <firemasterleo@outlook.com>
2025-09-16 15:42:00 +0100

    Update LatestReleases.vue

--- components/LatestReleases.vue
+++ components/LatestReleases.vue
@@ -22,7 +22,7 @@
                           width="1200"
                         height="800"
                     format="webp"
-                    :placeholder="[80, 80, 10, 30]" 
+                    :placeholder="[80, 80, 10, 130]" 
                     placeholder-class="my-placeholder"
                  class="blur-up"
 
